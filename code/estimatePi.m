f1a4c3c715d48a730803628571050d04e2dd0d9f
unknown <ericlee0803@yahoo.com>
2017-03-13 16:51:56 -0400

    took matlab example

--- code/estimatePi.m
+++ code/estimatePi.m
@@ -1,4 +1,6 @@
 % Estimate Pi in Serial
+function estimatePi()
+feature accel on
 tic;
 count = 0;      % count variable, start value set to zero
 n=1000000;
@@ -11,23 +13,27 @@ for i=1:n
     end
 end
 piapprox = 4*(count/n);
-toc;
+time = toc;
 disp('Value of Pi is:');
 disp(piapprox);
 
 % Estimate Pi with Two Workers
 parpool(2)
 tic;
-count = 0;      % count variable, start value set to zero
+      % count variable, start value set to zero
+n=1000000;
+count = zeros(2,1);
 parfor i=1:n 
     x=rand;
     y=rand;
     
     if (x^2+y^2 <=1)  %if pair is within the first quadrant of the unit circle
-        count = count +1;   % if yes, it increases the count by one
+        idx = mod(i,2);
+        count(idx) = count(idx) + 1;   % if yes, it increases the count by one
     end
 end
-piapprox = 4*(count/n);
+piapprox = 4*(sum(count)/n);
 toc;
 disp('Value of Pi is:');
 disp(piapprox);
+delete(gcp('nocreate'));
